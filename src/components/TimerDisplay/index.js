15d54b9605aaa549035ec57828767b239c557dee
Reuter Junior <reuter_junior@hotmail.com>
2020-10-14 17:29:28 -0300

    calma...

---
+++ src/components/TimerDisplay/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import {dateTimeNow} from '../componentsUtils';
+
+import Typography from '@material-ui/core/Typography';
+
+export default function TimerDisplay() {
+  const calcTime = () => {}
+
+  
+}
--- src/pages/Timer/index.js
+++ src/pages/Timer/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import TimeDisplay from '../../components/TimerDisplay';
+
 export default function Timer() {
     return(
-        <></>
+        <div>
+            <TimeDisplay></TimeDisplay>
+        </div>
     );
 }
